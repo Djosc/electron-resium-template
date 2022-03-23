2bd179fa3b631363f59d13808eb8ea389e40288e
Djosc <davidjoscaldwell@gmail.com>
2022-03-23 17:29:00 -0400

    Install and initialize electron

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { BrowserWindow, app } = require('electron');
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		backgroundColor: 'white',
+		webPreferences: {
+			nodeIntegrations: false,
+			worldSafeExecuteJavaScript: true,
+			contextIsolation: true,
+		},
+	});
+
+	win.loadFile('src/index.html');
+}
+
+app.whenReady().then(createWindow);
